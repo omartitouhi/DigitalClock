3a93c7d4898f858ad8bb471e81b2071c425b08f3
omar <titouhiomar223@gmail.com>
2023-08-24 10:58:43 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function (){
+    function showTime(){
+        //get current time date
+        var date = new Date();
+
+        //get hours min and sec
+        var hours = date.getHours();
+        var min = date.getMinutes();
+        var sec = date.getSeconds();
+
+        //AM, PM
+        var session = "AM";
+
+        //time behavior
+        if(hours == 0){
+            hours = 12;
+        }
+
+        if(hours >=12){
+            session = "PM";
+        }
+
+        if(hours > 12){
+            hours = hours-12;
+        }
+
+        hours = hours < 10 ? "0" + hours :hours;
+        min = min < 10 ? "0" + min :min;
+        sec = sec < 10 ? "0" + sec :sec;
+
+    }
+})
